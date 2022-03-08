3bdfe0de782cdad08620dc22e7b90e1b40e61c41
Leandro Voltolino <leandro@voltolino.com>
2022-03-08 18:18:03 -0300

    added coins

--- src/js/scenes/sample_scene.ts
+++ src/js/scenes/sample_scene.ts
@@ -7,6 +7,10 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 export class SampleScene extends Phaser.Scene {
 
     private player: Phaser.Physics.Arcade.Sprite & { body: Phaser.Physics.Arcade.Body };
+    private score: integer = 0;
+    private score_label: Phaser.GameObjects.Text;
+    private coin_good: Phaser.Physics.Arcade.Sprite & { body: Phaser.Physics.Arcade.Body };
+    private coin_bad: Phaser.Physics.Arcade.Sprite & { body: Phaser.Physics.Arcade.Body };
 
     constructor() {
         super(sceneConfig);
@@ -14,10 +18,18 @@ export class SampleScene extends Phaser.Scene {
 
     public preload () {
         this.load.tilemapTiledJSON('level_01', 'assets/tilemaps/level_01.json');
-        this.load.image('tiles', 'assets/tilesets/tilesheet_complete.png');
-        this.load.spritesheet('player', 'assets/tilesets/player_walk.png', {
+        this.load.image('tiles', 'assets/spritesheets/tilesheet_complete.png');
+        this.load.spritesheet('player', 'assets/spritesheets/player_walk.png', {
             frameWidth: 32,
             frameHeight: 32
+        })
+        this.load.spritesheet('coin_good', 'assets/spritesheets/coin_16x16.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+        this.load.spritesheet('coin_bad', 'assets/spritesheets/coin_16x16-red.png', {
+            frameWidth: 16,
+            frameHeight: 16
         })
     }
 
@@ -33,14 +45,16 @@ export class SampleScene extends Phaser.Scene {
         const ground = map.createLayer(3, tiles, 0, 0)
         ground.setCollisionByExclusion([-1], true);
 
-        map.createLayer(4, tiles, 0, 0)
-
         this.player = this.physics.add.sprite(30, 50, 'player')
         this.player.setBounce(0.1);
         this.player.setGravityY(1000);
         this.player.setCollideWorldBounds(true);
-
         this.physics.add.collider(this.player, ground);
+
+        this.coin_good = this.physics.add.sprite(130, 80, 'coin_good')
+        this.coin_bad = this.physics.add.sprite(352, 144, 'coin_bad')
+        this.coin_good.name = 'good'
+        this.coin_bad.name = 'bad'
 
         this.anims.create({
             key: 'idle',
@@ -68,6 +82,37 @@ export class SampleScene extends Phaser.Scene {
             frameRate: 8,
             repeat: 0
         })
+
+        this.anims.create({
+            key: 'spin_good',
+            frames: this.anims.generateFrameNumbers('coin_good', {
+                frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            }),
+            frameRate: 18,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'spin_bad',
+            frames: this.anims.generateFrameNumbers('coin_bad', {
+                frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            }),
+            frameRate: 18,
+            repeat: -1
+        })
+
+        this.physics.add.collider(this.player, [this.coin_good, this.coin_bad], function (player, coin) {
+            if (coin.name == 'bad') {
+                this.score -= 1
+            } else {
+                this.score += 1
+            }
+
+            coin.destroy();
+        });
+
+        this.coin_good.play('spin_good', true);
+        this.coin_bad.play('spin_bad', true);
 
     }
 
